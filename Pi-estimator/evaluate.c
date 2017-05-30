1981a349ae3905390bdc8a1bf92e67542c20b3ad
Evan Wickenden <eww2119@columbia.edu>
2017-05-29 22:09:18 -0400

    working pi estimator

--- Pi-estimator/evaluate.c
+++ Pi-estimator/evaluate.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
 
 struct coord
 {
@@ -12,9 +13,17 @@ struct coord
 };
 
 #define NCOORDS 400
-#define NTHREADS 16
+#define NTHREADS 10
 
 #define LOG(msg, ...)   fprintf(stderr, "log: " msg "\n", ##__VA_ARGS__)
+
+
+#ifdef __DEBUG__
+#define DEBUG(msg, ...)     fprintf(stderr, "debug: " msg "\n", ##__VA_ARGS__)
+#else
+#define DEBUG(msg, ...)     {};
+#endif
+
 
 struct coord coords[NCOORDS];
 
@@ -24,51 +33,68 @@ sem_t empty;
 sem_t full;
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-struct coord *start;
-struct coord *end;
+int start;
+int end;
 
-long long n_runs;
-long long n_accepts;
+long long n_runs = 0;
+long long n_accepts = 0;
 pthread_mutex_t stats = PTHREAD_MUTEX_INITIALIZER;
-
+    
 
 #define generate_random() (rand() / (float) RAND_MAX);
 
 
-void *execute(void *null)
+void *execute(void *num)
 {
-    struct coord *index;
     struct coord coord;
     int do_accept;
 
-    LOG("entering execute");
+    int n = (int) num;
+
+    LOG("thread %d: entering execute", n);
 
 produce:
+
+    DEBUG("produce");
+
     coord.x = generate_random();
     coord.y = generate_random();
 
     if (sem_trywait(&empty) != 0) 
     {
-        if (errno == EAGAIN) goto consume;
+        if (errno == EAGAIN) 
+        {
+            goto consume;
+        }
         else goto end;
     }
+
     pthread_mutex_lock(&lock); /* spinlock? */
-    *++end = coord;
+    coords[end] = coord;
+    end = (end + 1) % NCOORDS;
     pthread_mutex_unlock(&lock);
     sem_post(&full);
+
 
     goto produce;
 
 
 consume:
+
+    DEBUG("consume");
+
     if (sem_trywait(&full) != 0) 
     {
-        if (errno == EAGAIN) goto produce;
+        if (errno == EAGAIN) 
+        {
+            goto produce;
+        }
         else goto end;
     }
 
     pthread_mutex_lock(&lock);
-    coord = *++start;
+    start = (start + 1) % NCOORDS;
+    coord = coords[start];
     pthread_mutex_unlock(&lock);
     sem_post(&empty);
 
@@ -82,13 +108,38 @@ consume:
 
     goto consume;
 
-    /* terminate execution synchronously by destroying semaphores */
 end: 
     LOG("exeting execute");
     return NULL;
 }
 
 
+
+void terminate(int null)
+{
+    int i;
+    LOG("Ctl-C terminating estimation");
+
+    //sem_destroy(&empty);
+    //sem_destroy(&full);
+
+    for (i = 0; i < NTHREADS; i++)
+    {
+        pthread_cancel(workers[i]);
+    }
+
+    /*
+    for (i = 0; i < NTHREADS; i++)
+    {
+        pthread_join(workers[i], NULL);
+    }
+    */
+
+    printf("Estimate for pi: N_TOTAL = %lld; N_ACCEPTED = %lld; pi ~ %f \n", 
+            n_runs, 
+            n_accepts,
+            4 * n_accepts / (float) n_runs); 
+}
 
 
 int main()
@@ -103,22 +154,22 @@ int main()
             0,
             0);
 
-    end = start = coords;
+    end = start = 0;
 
     for (i = 0; i < NTHREADS; i++)
+    {
         pthread_create(workers + i, NULL, execute, NULL);
+    }
 
-    sleep(10);
-    
-    sem_destroy(&empty);
-    sem_destroy(&full);
+    struct sigaction act;
+    act.sa_handler = terminate;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = SA_RESTART;
+    sigaction(SIGINT, &act, NULL);
 
-    for (i = 0; i < NTHREADS; i++)
-        pthread_join(workers[i], NULL); 
+    pthread_join(workers[0], NULL);
 
-    printf("Estimate for pi: N_TOTAL = %lld; N_ACCEPTED = %lld;\n", 
-            n_runs, 
-            n_accepts); 
+    terminate(0);
 }
 
 
