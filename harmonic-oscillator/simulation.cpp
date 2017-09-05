4a4f2d725246afcfe1b474c5f99739010d49b9d8
Evan Wynn Wickenden <eww2119@columbia.edu>
2017-09-04 22:31:21 -0400

    snapshot

--- harmonic-oscillator/simulation.cpp
+++ harmonic-oscillator/simulation.cpp
@@ -2,61 +2,78 @@
 #include <math.h>
 
 #include "path-integral.h"
+#include "data.h"
 
+/* new functions */
 
-struct X1X2 : public Observable
+struct Measurement
 {
-	int i, j;
-	double data;
-
-	X1X2(int i, int j) : i(i), j(j), data(0) { }
-
-	void operator()(const Lattice& lattice)
-	{
-		data += lattice[i] * lattice[j];
-	}
+    Observable &o;
+    Measurement(Observable& o) : o(o) {};
 };
+
+
+/*    */
+
+
 
 struct Correlation : public Observable
 {
 	int index, offset;
 	int count;
-	double *data;
+	Data **data_sets;
 
-	Correlation(int index, int offset) :
+	Correlation(int index, int offset, int data_set_size) :
 		index(index), offset(offset), count(0),
-		data(new double[offset])
+        data_sets(new Data *[offset])
 	{
 		int i;
 		for (i = 0; i < offset; i++)
-			data[i] = 0;
+        {
+           data_sets[i] = new Data(data_set_size);
+        }
 	}
-	~Correlation(){ delete[] data; }
+	~Correlation()
+    { 
+        int i;
+        for (i = 0; i < offset; i++)
+        {
+            delete data_sets[i];
+        }
+        delete[] data_sets;
+    }
 
 	void operator()(const Lattice& lattice)
 	{
 		int i;
 		for (i = 0; i < offset; i++)
-		{
-			data[i] += lattice[index] * lattice[index + i];
-		}
-		count++;
+			data_sets[i]->data[count] = lattice[index] * lattice[index + i];
+        count++;
 	}
 
-	double operator[](int i) { return data[i] / (float) count; }
+    void analyze_data_sets()
+    {
+        int i;
+        for (i = 0; i < offset; i++)
+            data_sets[i]->analyze_data_set();
+    }
 
 	friend std::ostream& operator<<(std::ostream& s, Correlation& c)
 	{
 		int i;
-		s << "correlation measurement starting at index " << c.index << std::endl;
+        s << "\n means: " << std::endl;
 		for (i = 0; i < c.offset; i++)
 		{
-			s << c[i] << "\n";
+            s << c.data_sets[i]->mean << std::endl;
+		}
+        s << "\n std devs: " << std::endl;
+		for (i = 0; i < c.offset; i++)
+		{
+            s << c.data_sets[i]->std_dev << std::endl;
 		}
 		return s;
 	}
 };
-
 
 struct HO : public DeltaAction
 {
@@ -75,25 +92,25 @@ struct HO : public DeltaAction
 int main()
 {
 	const long steps = 500000000;
-	const double tau = 1 / (float) 40;
-	const double omega = 3;
-	const int lattice_size = 200;
+	const double tau = 0.2;
+	const double omega = 1;
+	const int lattice_size = 100;
 
-	/* analytic solution for 2 elt lattice */
-//	double prediction = 1 / ( tau * omega * omega * (4 + tau * tau * omega * omega ) );
-//	std::cout << "prediction = " << prediction << std::endl;
+//    std::cout << 1 / (tau * omega * omega * 4) << std::endl;
 
 	Lattice lattice(lattice_size);
-	Correlation cor(20, lattice_size + 1);
+	Correlation cor(0, lattice_size, steps / lattice_size);
 	HO ho(tau, omega);
 
 	int i;
 	for (i = 0; i < lattice_size; i++)
 	{
-		lattice[i] = 0;
+		lattice[i] = 0.35;
 	}
 
 	path_integral(lattice, steps, 0.09, ho, cor);
+
+    cor.analyze_data_sets();
 
 	std::cout << cor;
 }
